ec37ce546844ce983fec002c09972c63b0fc33d4
Christopher <mail@christophermaier.com.au>
2017-11-20 12:07:28 +1000

    01 completed

--- src/1-converter.js
+++ src/1-converter.js
@@ -2,21 +2,49 @@ function convertCelsiusToFahrenheit(celsius) {
   return (celsius * 1.8) + 32
 }
 
+function convertFahrenheitToCelsius(fahrenheit) {
+  return (fahrenheit - 32) * .5556
+}
+
+function formatCelsius(a) {
+  return (Math.round(a) + '°C')
+}
+
+function formatFahrenheit(a) {
+  return (Math.round(a) + '°F')
+}
+
 let fahrenheit
+
+let celsius
+
+let weeklyTemps = [32, 15, 18, 22, 44, 16, 44]
 
 // Monday
 
 const mondayTemperature = 24
 console.log(`On Monday it is ${mondayTemperature}°C`)
-fahrenheit = convertCelsiusToFahrenheit(mondayTemperature)
-console.log(`The Americans would think of that as ${fahrenheit}°F`)
+fahrenheit = formatFahrenheit(convertCelsiusToFahrenheit(mondayTemperature))
+console.log(`The Americans would think of that as ${fahrenheit}`)
 
 // Tuesday
 
 const tuesdayTemperature = 31
 console.log(`On Tuesday it is ${tuesdayTemperature}°C`)
-fahrenheit = convertCelsiusToFahrenheit(tuesdayTemperature)
-console.log(`The Americans would think of that as ${fahrenheit}°F`)
+fahrenheit = formatFahrenheit(convertCelsiusToFahrenheit(tuesdayTemperature))
+console.log(`The Americans would think of that as ${fahrenheit}`)
+
+// Wednesday
+
+const wednesdayTemperature = 99
+console.log(`On Wednesday it is ${wednesdayTemperature}°F`)
+celsius = formatCelsius(convertFahrenheitToCelsius(wednesdayTemperature))
+console.log(`The Aussies would think of that as ${celsius}`)
+
+// Weekly temperatures
+
+console.log(`This week's temperatures are going to be ${weeklyTemps}`)
+console.log(`The highest temperature this week will be: ${Math.max(...weeklyTemps) + '°'}`)
 
 /*
 
