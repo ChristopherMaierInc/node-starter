289b942e693914f7f34dcf9c79d152e72747011c
Christopher <mail@christophermaier.com.au>
2017-11-20 13:59:56 +1000

    03 Complete

--- src/2-numberplate.js
+++ src/2-numberplate.js
@@ -1,14 +1,24 @@
 function formatVanityPlate(string) {
   let words = string.split(' ')
+  console.log(words)
   words = words.map(word => word.toUpperCase())
+  words = words.replace(/'for'/gi, '4')
+  words = words.replace(/'to'/gi, '2')
   return words.join('')
 }
+//
+// function replaceFor {
+//   str.replace("for","4")
+// }
 
 const one = 'I like money'
 console.log(`Number plate: ${ formatVanityPlate(one) }`)
 
 const two = 'Cats for life'
 console.log(`Number plate: ${ formatVanityPlate(two) }`)
+
+const three = 'I am late'
+console.log(`Number plate: ${ formatVanityPlate(three) }`)
 
 
 /*
--- src/3-adder.js
+++ src/3-adder.js
@@ -9,7 +9,6 @@ function sum(numbers) {
 console.log(sum([12, 5])) // => 17
 console.log(sum([5, 7, 1])) // => 13
 
-
 function sumNumbersString(numbersString) {
   const numbers = numbersString
     .split('+')
@@ -18,11 +17,32 @@ function sumNumbersString(numbersString) {
   return sum(numbers)
 }
 
-
 console.log(sumNumbersString('12 + 5')) // => 17
 console.log(sumNumbersString('5 + 7 + 1')) // => 13
 console.log(sumNumbersString('1 + 2 + 3 + 4 + 5')) // => 15
 
+function multiple(numbers) {
+  let total = 1
+  numbers.forEach(number => {
+    total = total * number
+  })
+  return total
+}
+
+console.log(multiple([5, 5])) // => 25
+console.log(multiple([5, 5, 3])) // => 75
+
+function multipleNumbersString(numbersString) {
+  const numbers = numbersString
+    .split('*')
+    .map(string => parseFloat(string))
+
+  return multiple(numbers)
+}
+
+console.log(multipleNumbersString('12 * 5')) // => 17
+console.log(multipleNumbersString('5 * 7 * 1')) // => 13
+console.log(multipleNumbersString('1 * 2 * 3 * 4 * 5')) // => 15
 
 /*
 
