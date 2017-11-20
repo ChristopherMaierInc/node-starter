009cc554fb56df5d3a4abf6783c36537ece92bf1
Christopher <mail@christophermaier.com.au>
2017-11-20 12:12:43 +1000

    01 refactored

--- src/1-converter.js
+++ src/1-converter.js
@@ -23,28 +23,28 @@ let weeklyTemps = [32, 15, 18, 22, 44, 16, 44]
 // Monday
 
 const mondayTemperature = 24
-console.log(`On Monday it is ${mondayTemperature}°C`)
+console.log(`On Monday it is ${ mondayTemperature }°C`)
 fahrenheit = formatFahrenheit(convertCelsiusToFahrenheit(mondayTemperature))
-console.log(`The Americans would think of that as ${fahrenheit}`)
+console.log(`The Americans would think of that as ${ fahrenheit }`)
 
 // Tuesday
 
 const tuesdayTemperature = 31
-console.log(`On Tuesday it is ${tuesdayTemperature}°C`)
+console.log(`On Tuesday it is ${ tuesdayTemperature }°C`)
 fahrenheit = formatFahrenheit(convertCelsiusToFahrenheit(tuesdayTemperature))
-console.log(`The Americans would think of that as ${fahrenheit}`)
+console.log(`The Americans would think of that as ${ fahrenheit }`)
 
 // Wednesday
 
 const wednesdayTemperature = 99
-console.log(`On Wednesday it is ${wednesdayTemperature}°F`)
+console.log(`On Wednesday it is ${ wednesdayTemperature }°F`)
 celsius = formatCelsius(convertFahrenheitToCelsius(wednesdayTemperature))
-console.log(`The Aussies would think of that as ${celsius}`)
+console.log(`The Aussies would think of that as ${ celsius }`)
 
 // Weekly temperatures
 
-console.log(`This week's temperatures are going to be ${weeklyTemps}`)
-console.log(`The highest temperature this week will be: ${Math.max(...weeklyTemps) + '°'}`)
+console.log(`This week's temperatures are going to be ${ weeklyTemps }`)
+console.log(`and the highest temperature will be: ${ Math.max(...weeklyTemps) + '°' }`)
 
 /*
 
